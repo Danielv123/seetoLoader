eda6202058793ef2eabe439e0d4700c847bbae23
Daniel <tranen.bananen@gmail.com>
2015-04-16 10:28:48 +0200

    Removed stuff not needed, project done.

--- loader.php
+++ loader.php
@@ -1,12 +1,8 @@
 <?php 
-for ($x = 10000; $x <= 11000; $x++) {
+for ($x = 23001; $x <= 25000; $x++) {
     echo "seeto.no/gem/servlet/getGemObject?id=$x <br>";
-	$file = 'seeto.txt';
-// The new person to add to the file
-$person = "seeto.no/gem/servlet/getGemObject?id=$x\n";
-// Write the contents to the file, 
-// using the FILE_APPEND flag to append the content to the end of the file
-// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+	$y = 'seeto.txt';
+$z = "seeto.no/gem/servlet/getGemObject?id=$x\n";
+file_put_contents($y, $z, FILE_APPEND | LOCK_EX);
 } 
 ?>
